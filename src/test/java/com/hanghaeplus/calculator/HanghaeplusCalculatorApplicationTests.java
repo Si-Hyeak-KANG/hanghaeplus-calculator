ee1459323208fb238a3cf47be247afed10caa93b
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-09-28 15:54:07 +0900

    first init, 환경구축

---
+++ src/test/java/com/hanghaeplus/calculator/HanghaeplusCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghaeplus.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeplusCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
